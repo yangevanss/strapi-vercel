7d968e77e5dea91c08a67a13faabae8ad20d6d7f
Evan <yangevanss@gmail.com>
2024-09-29 16:39:19 +0800

    feat(): vercel 測試

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
-const strapi = require("@strapi/strapi");
-
-strapi.createStrapi({ distDir: "./dist" }).start();
+try { 
+  console.log(123)
+  
+  const strapi = require("@strapi/strapi");
+  
+  strapi.createStrapi({ distDir: "./dist" }).start();
+}catch(e){
+  console.log(e);
+}
